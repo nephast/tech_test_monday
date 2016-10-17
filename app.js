b0a25cd37e936f72e92ce0174efb395193469c42
Stéphan Gaud <stephgoeseast@gmail.com>
2016-10-17 15:34:51 +0100

    get and set, array with key and value

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
+var data = [];
+
 app.get('/get', function (req, res) {
-  var data = ["test", "hello"];
   res.render('get', {data: data});
 });
 
 app.post('/set', function (req, res) {
-  console.log(req.body);
-  res.send("post route");
+  var key =  req.body.key;
+  var value = req.body.value;
+  data.push(key);
+  data.push(value);
+  res.redirect("/get");
 });
 
 app.listen(4000, function () {
