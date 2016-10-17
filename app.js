c92fbdc5ccf3fa58af3e08691deb1872dd78d03a
Stéphan Gaud <stephgoeseast@gmail.com>
2016-10-17 17:12:52 +0100

    prepa

--- app.js
+++ app.js
@@ -15,7 +15,21 @@ app.post('/set', function (req, res) {
   var value = req.body.value;
   data.push(key);
   data.push(value);
-  res.redirect("/get");
+  res.redirect("/result");
+});
+
+app.post('/get', function (req, res) {
+  var result = req.body.result;
+  if (result == data[0]) {
+  //  res.render('get', {data: data});
+    res.render('get', {data: data[1]};
+  } else {
+  res.redirect("/result");
+  }
+});
+
+app.get('/result', function (req, res) {
+  res.render("result");
 });
 
 app.listen(4000, function () {
